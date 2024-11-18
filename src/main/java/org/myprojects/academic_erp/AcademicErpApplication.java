c7b9ac14a3e10246155c37a51e05c4b155b3cf1b
Rajan Khade <rajankhade31@gmail.com>
2024-11-19 03:10:18 +0530

    init: setup

---
+++ src/main/java/org/myprojects/academic_erp/AcademicErpApplication.java
@@ -0,0 +1,11 @@
+package org.myprojects.academic_erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicErpApplication {
+
+    public static void main(String[] args) {SpringApplication.run(AcademicErpApplication.class, args);}
+
+}
---
+++ src/test/java/org/myprojects/academic_erp/AcademicErpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myprojects.academic_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
