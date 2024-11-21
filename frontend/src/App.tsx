9d2f06a6dfb8462b8c965a3fe3b8e847976c6818
Rajan Khade <rajankhade31@gmail.com>
2024-11-22 04:08:49 +0530

    feat: react routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,51 @@
 import './App.css';
+import {
+	createBrowserRouter,
+	RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Student from './pages/Student';
+import Employee from './pages/Employee';
+import Navbar from './components/Layout';
+import Layout from './components/Layout';
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{
+				path: "/",
+				element: <Home />
+			},
+			{
+				path: "/login",
+				element: <Login />
+			},
+			{
+				path: "/register",
+				element: <Register />
+			},
+			{
+				path: "/student",
+				element: <Student />
+			},
+			{
+				path: "/employee",
+				element: <Employee />
+			}
+		]
+	}
+]);
 
 function App() {
-  return (
-   <div>
-    <h1>Home</h1>
-   </div>
-  );
+	return (
+		<div>
+			<RouterProvider router={router} />
+		</div>
+	);
 }
 
 export default App;
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+            <Link to="/student">Student</Link>
+            <Link to="/employee">Employee</Link>
+        </nav>
+    );
+}
+
+export default Navbar;
